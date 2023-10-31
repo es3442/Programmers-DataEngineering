f894e5e9b5af25ef507126b18d0bc1e9cd96868c
es3442 <{es3442@naver.com}>
2023-11-01 00:54:25 +0900

    dir구조변경

--- mysite/polls/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "polls"
-urlpatterns = [
-    path('', views.index, name="index"),
-    path('some_url', views.some_url),
-    path('<int:question_id>/', views.detail, name='detail')
-]
