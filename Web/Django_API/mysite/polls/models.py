bca3bf327cccb662560992e7793111b7c307e02e
es3442 <es3442@naver.com>
2023-11-03 23:53:12 +0900

    djnago project final commit

--- Web/Django_API/mysite/polls/models.py
+++ Web/Django_API/mysite/polls/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib import admin
+from django.contrib.auth.models import User
 import datetime
 
 
@@ -23,9 +24,21 @@ class Question(models.Model):
 
 class Choice(models.Model):
     question = models.ForeignKey(
-        Question, on_delete=models.CASCADE)  # Foreign_key
+        Question, related_name='choices', on_delete=models.CASCADE)  # Foreign_key
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):  # 모델 객체 목록에서, 제목과 날짜가 표시
         return f'[{self.question.question_text}] : {self.choice_text}'
+
+
+class Vote(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    voter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['question', 'voter'], name='unique_voter_for_questions')
+        ]
